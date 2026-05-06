0f18597d0158c9110e7a98afde2cc5c14a688fdb
Muhammad Abdullah <168014075+itxAkaza@users.noreply.github.com>
2026-05-06 14:20:37 +0500

    App Colors

--- lib/resources/colors/app_colors.dart
+++ lib/resources/colors/app_colors.dart
@@ -4,23 +4,20 @@ class AppColors {
 
 
 
+  // App Colors
+
   static const Color primaryDarkTeal = Color(0xFF1C4A5A);
 
-  /// Used for active states, key focus indicators, badges, and warnings
   static const Color accentOrange = Color(0xFFFFB057);
 
-  /// Used for the main screen backgrounds
   static const Color backgroundLightPeach = Color(0xFFFFEEDB);
 
-  /// Used for containers, bottom sheets, list items, and input fields
   static const Color cardOffWhite = Color(0xFFFAFAFA);
 
   // --- Utility Colors ---
 
-  /// Used for subtle text, labels, and borders
   static const Color textGrey = Color(0xFF686868);
 
-  /// Used for read-only fields or disabled states (like the mapped GatePass data)
   static const Color readOnlyBg = Color(0xFFF0F0F0);
 
 }
